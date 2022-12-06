22a009f37adf4a5115e237adc1aa825b7473ce3e
Jihwan Kim <jihwan.kim@purple.io>
2022-12-06 16:24:55 +0900

    Deploy : gh-pages 배포

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "http://fankimm.github.io/keyCode";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: "https://github.com/fankimm/keyCode",
+  assetPrefix: !debug ? `/${repository}/` : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
