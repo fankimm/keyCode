fef542ccb755a07a51dd85e2b7cb4f54fc87b7cd
Jihwan Kim <jihwan.kim@purple.io>
2022-12-06 16:43:14 +0900

    Deploy : gh-pages 배포

--- out/_next/static/igo4ZwR0P2hdtpgyPRJ0S/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/99b2eada55ebb9a3.css","static/chunks/pages/index-d415f8187d865b2c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
