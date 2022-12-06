46e835f147e6ad0cdba1894a68bfb104ee11a482
Jihwan Kim <jihwan.kim@purple.io>
2022-12-06 16:36:59 +0900

    Update : 배포 환경 수정

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
-const repository = "http://fankimm.github.io/keyCode";
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: !debug ? `/${repository}/` : "",
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
